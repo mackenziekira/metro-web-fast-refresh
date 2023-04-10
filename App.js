48d7b58c310539c5b98697576c6fd6c1551aab48
mackenzie <mackenzie@joinrivet.com>
2023-04-10 16:03:34 -0700

    add imported component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import "@expo/metro-runtime";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Greeting from "./Greeting";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Greeting />
       <StatusBar style="auto" />
     </View>
   );
---
+++ Greeting.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const Greeting = () => {
+  return <Text>Open up App.js to start working on your app!</Text>;
+};
+
+export default Greeting;
